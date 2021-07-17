0199a11d22bcc97f32159b2c0e1c527b18c86035
Mrutyunjay Dash <mdash555@gmail.com>
2021-07-17 19:19:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default App;
